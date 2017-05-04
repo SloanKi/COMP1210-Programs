5d2428147d32240897e27aac6e1200ec50a1e2ff
SloanTheSloth <sloan.kiechel@gmail.com>
2017-05-03 21:30:15 -0500

    Add files via upload
    First ever programs in Java. JLetter prints out a large block letter "J" composed of characters 'J' 'A' 'V' 'A' in a pattern.
    Life Goals prints out my goals.

---
+++ Program01/JLetter.java
@@ -0,0 +1,28 @@
+/**
+* This program prints out a large block letter 'J' composed
+* of the characters 'J', 'A', 'V', 'A', n a pattern.
+*
+* @author Sloan Kiechel
+* @version 1/21/2016
+*/
+public class JLetter
+{
+   /**
+   * Prints to std output.
+   *
+   * @param args Command line arguments (not used).
+   */
+   public static void main(String[] args)
+   {
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("J     JAVA");
+      System.out.println("JA    JAVA");
+      System.out.println(" JAVAJAVA");
+      System.out.println("  JAVAJA");
+   }
+}
---
+++ Program01/MyLifeGoals.java
@@ -0,0 +1,35 @@
+/**
+* This program lists my short term, midterm, and long term life goals.
+*
+* @author Sloan Kiechel
+* @version 1/21/2016
+*/
+public class MyLifeGoals
+{
+ /**
+ * Prints to std output.
+ *
+ * @param args Command line arguments (not used).
+ */
+   public static void main(String[] args)
+   {
+      //Name
+      System.out.println("Sloan Kiechel");
+      System.out.println();
+      //Short Term Goals
+      System.out.println("My short term goal is to get through this semester" 
+                           + " at Auburn with above a 3.0 GPA to keep "
+                           + "all of my academic scholarships for next year.");
+      //Medium Term Goals
+      System.out.println("My medium term goal is to get through all four"
+                           + " years of college with as little debt and"
+                           + " as much work experience as possible.");
+      //Long Term Goals
+      System.out.println("My long term goal is get a job with "
+                           + "computer science, hopefully with video "
+                           + "games and virutal reality. I want to use "
+                           + "virtual reality to help overcome mental "
+                           + "illnesses.");
+   
+   }
+}
